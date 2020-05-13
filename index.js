07e4ca917c058a2d26ad03b3883df45f48a85f57
Umrzoq  Thinkland <umrzoq@thinkland.uz>
2020-05-14 00:39:53 +0500

    It is react js boilerplate

--- index.js
+++ index.js
@@ -4,32 +4,59 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-const config = require('./config/keys')
+const config = require("./config/keys");
 
-const { User } = require('./models/user');
+const { User } = require("./models/user");
 
-
-mongoose.connect(config.mongoURI,
-    { useNewUrlParser: true }).then(() => console.log("DB connected"))
-                                .catch((err) => console.log(err));
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.json({ "hello ~": "Hi ~~ Umrzoq" });
+app.get("/", (req, res) => {
+  res.json({ "hello ~": "Hi ~~ Umrzoq" });
 });
 
-app.post('/api/users/register', (req, res) => {
-    const user = new User(req.body);
-    user.save((err, userData) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-          success: true,
-        });
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+      userData: doc,
     });
-    
+  });
+});
+
+app.post("/api/user/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "wrong password",
+        });
+      }
+    });
+
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+      });
+    });
+  });
 });
 
 app.listen(5000);
